277c8085145375e5c2ce3626b71080043617507f
upbeatDiamond <112040252+upbeatDiamond@users.noreply.github.com>
2024-11-11 15:20:04 -0500

    20241111-1520
    Condensed and enhanced the CutsceneTrigger condition comparison code; comparing int to String is... annoying.

--- overworld/cutscene_trigger.gd
+++ overworld/cutscene_trigger.gd
@@ -18,6 +18,18 @@ extends Area2D
 ## This should not be backed up, it should be relatively constant
 @export var one_shot := true
 
+enum Operator{
+	EQUAL,
+	UNEQUAL,
+	LT_EQUAL,
+	GT_EQUAL,
+	LESS_THAN,
+	GREATER_THAN,
+}
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -72,110 +84,70 @@ func _match_conditions() -> bool:
 		match str(conditions[key]).strip_edges().split(" ")[0]: 
 			"==": ## Equals
 				compare = str(conditions[key]).split("==",true,1)[1]
-				if val == null or val == "<null>":
-					val = 0;
-				if compare == null or compare == "<null>":
-					compare = 0;
-				if str(val).strip_edges().is_valid_int() and str(compare).strip_edges().is_valid_int():
-					invalidated = not (str(val).to_int() == str(compare).to_int())
-				elif str(val).strip_edges().is_valid_float() and str(compare).strip_edges().is_valid_float():
-					invalidated = not (str(val).to_float() == str(compare).to_float())
-				else:
-					invalidated = ( typeof(val) != typeof(compare) )
+				invalidated = not _test_relation(val, Operator.EQUAL, compare)
 			"=": ## Equals 2
 				compare = str(conditions[key]).split("=",true,1)[1]
-				if val == null or val == "<null>":
-					val = 0;
-				if compare == null or compare == "<null>":
-					compare = 0;
-				if str(val).strip_edges().is_valid_int() and str(compare).strip_edges().is_valid_int():
-					invalidated = not (str(val).to_int() == str(compare).to_int())
-				elif str(val).strip_edges().is_valid_float() and str(compare).strip_edges().is_valid_float():
-					invalidated = not (str(val).to_float() == str(compare).to_float())
-				else:
-					invalidated = ( typeof(val) != typeof(compare) )
+				invalidated = not _test_relation(val, Operator.EQUAL, compare)
 			
 			"!=": ## Not Equals
 				compare = str(conditions[key]).split("!=",true,1)[1]
-				if val == null or val == "<null>":
-					val = 0;
-				if compare == null or compare == "<null>":
-					compare = 0;
-				if str(val).strip_edges().is_valid_int() and str(compare).strip_edges().is_valid_int():
-					invalidated = not (str(val).to_int() != str(compare).to_int())
-				elif str(val).strip_edges().is_valid_float() and str(compare).strip_edges().is_valid_float():
-					invalidated = not (str(val).to_float() != str(compare).to_float())
-				else:
-					invalidated = ( typeof(val) == typeof(compare) and val == compare)
-			
+				invalidated = not _test_relation(val, Operator.UNEQUAL, compare)
 			
 			"<": ## Less Than
 				compare = str(conditions[key]).split("<",true,1)[1]
-				if val == null or val == "<null>":
-					val = 0;
-				if compare == null or compare == "<null>":
-					compare = 0;
-				if str(val).strip_edges().is_valid_int() and str(compare).strip_edges().is_valid_int():
-					invalidated = not (str(val).to_int() < str(compare).to_int())
-				elif str(val).strip_edges().is_valid_float() and str(compare).strip_edges().is_valid_float():
-					invalidated = not (str(val).to_float() < str(compare).to_float())
-				else:
-					invalidated = ( typeof(val) != typeof(compare) )
-					print("Warning! Non-integers compared! ", val, "<", compare)
+				invalidated = not _test_relation(val, Operator.LESS_THAN, compare)
 			
 			">": ## Greater Than
 				compare = str(conditions[key]).split(">",true,1)[1]
-				if val == null or val == "<null>":
-					val = 0;
-				if compare == null or compare == "<null>":
-					compare = 0;
-				if val is int:
-					invalidated = not (val > compare.to_int())
-				elif val is float:
-					invalidated = not (val > compare.to_float())
-				else:
-					invalidated = ( typeof(val) != typeof(compare) )
-					print("Warning! Non-integers compared! ", val, ">", compare)
+				invalidated = not _test_relation(val, Operator.GREATER_THAN, compare)
 			
 			"<=": ## Less Than or Equals
 				compare = str(conditions[key]).split("<=",true,1)[1]
-				if val == null or val == "<null>":
-					val = 0;
-				if compare == null or compare == "<null>":
-					compare = 0;
-				if val is int:
-					invalidated = not (val <= compare.to_int())
-				elif val is float:
-					invalidated = not (val <= compare.to_float())
-				else:
-					invalidated = ( typeof(val) != typeof(compare) )
-					print("Warning! Non-integers compared! <=")
+				invalidated = not _test_relation(val, Operator.LT_EQUAL, compare)
 			
 			">=": ## Greater Than or Equals
-				compare = str(conditions[key]).split("==",true,1)[1]
-				if val == null or val == "<null>":
-					val = 0;
-				if compare == null or compare == "<null>":
-					compare = 0;
-				if val is int:
-					invalidated = not (val >= compare.to_int())
-				elif val is float:
-					invalidated = not (val >= compare.to_float())
-				else:
-					invalidated = ( typeof(val) != typeof(compare) )
-					print("Warning! Non-integers compared! >=")
+				compare = str(conditions[key]).split(">=",true,1)[1]
+				invalidated = not _test_relation(val, Operator.GT_EQUAL, compare)
 			
 			_: ## Default, same as '==' for now...
 				compare = str(conditions[key])#.split("==",true,1)[1]
-				if str(val).strip_edges().is_valid_int() and str(compare).strip_edges().is_valid_int():
-					invalidated = not (str(val).to_int() == str(compare).to_int())
-				elif str(val).strip_edges().is_valid_float() and str(compare).strip_edges().is_valid_float():
-					invalidated = not (str(val).to_float() == str(compare).to_float())
-				else:
-					invalidated = ( typeof(val) != typeof(compare) or val == compare)
-		
+				invalidated = not _test_relation(val, Operator.EQUAL, compare)
+				
 		print("cutscene trigger, test relation, ", val, " ={ ", !invalidated ," }= ", compare)
 		
 		if invalidated:
 			return false;
 	return true;
+
+
+func _test_relation(var1, operator:Operator, var2) -> bool:
+	
+	if var1 == null or (var1 is String and var1 == "<null>"):
+		var1 = 0;
+	
+	if var2 == null or (var2 is String and var2 == "<null>"):
+		var2 = 0;
+	
+	if typeof(var1) == typeof(var2):
+		return var1 == var2
+	elif str(var1).strip_edges().is_valid_int() and str(var2).strip_edges().is_valid_int():
+		return _process_relation(str(var1).to_int(), operator, str(var2).to_int())
+	elif str(var1).strip_edges().is_valid_float() and str(var2).strip_edges().is_valid_float():
+		return _process_relation( str(var1).to_float(), operator, str(var2).to_float())
+	else:
+		return _process_relation( str(var1), operator, str(var2) )
+
+func _process_relation(var1, operator:Operator, var2) -> bool:
+	match operator:
+		Operator.UNEQUAL:
+			return var1 != var2
+		Operator.LT_EQUAL:
+			return var1 <= var2
+		Operator.GT_EQUAL:
+			return var1 >= var2
+		Operator.LESS_THAN:
+			return var1 < var2
+		Operator.GREATER_THAN:
+			return var1 > var2
+		_:
+			return var1 == var2
