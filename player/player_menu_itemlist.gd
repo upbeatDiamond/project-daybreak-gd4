500388a0fc19e29ba503c9e01bbcba2f03ceaca3
upbeatDiamond <upbeatDiamond@gmail.com>
2024-12-09 21:42:04 -0500

    Start convert player menu to ItemList

---
+++ player/player_menu_itemlist.gd
@@ -0,0 +1,14 @@
+extends ItemList
+
+@export var item_submenu_links := [
+	PlayerMenu.ScreenListing.PAUSE_MENU,
+	PlayerMenu.ScreenListing.PARTY_SCREEN,
+	PlayerMenu.ScreenListing.INVENTORY,
+	PlayerMenu.ScreenListing.APP_HOME,
+	PlayerMenu.ScreenListing.PROFILE,
+	PlayerMenu.ScreenListing.CAMP,
+	PlayerMenu.ScreenListing.SETTINGS,
+	PlayerMenu.ScreenListing.SAVE,
+	PlayerMenu.ScreenListing.CLOSED,
+	PlayerMenu.ScreenListing.QUIT_GAME
+]
